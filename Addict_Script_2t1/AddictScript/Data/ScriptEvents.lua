c9e36670f93732a322011ca9c01e0d28b7851f3f
Addict Script <122180726+Addict0919@users.noreply.github.com>
2023-01-12 18:13:20 +0000

    Add files via upload

---
+++ Addict_Script_2t1/AddictScript/Data/ScriptEvents.lua
@@ -0,0 +1,10 @@
+return {
+	["Kick 1"] = 1268038438,
+	["Breakup 1"] = 915462795,
+	["Infinite Loop"] = -904555865,
+	["Script Teleport"] = 879177392,
+	["Give Collectibles"] = 697566862,
+	["Interior Invite"] = -1891171016,
+	["Vehicle Kick"] = 3580698306,
+	["Transaction Error"] = 3526858346
+}
