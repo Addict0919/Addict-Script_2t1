a2a8315e8328f926f298dca6e5b793a5c358dd91
Addict Script <122180726+Addict0919@users.noreply.github.com>
2023-02-02 14:55:43 +0000

    Add files via upload

---
+++ Addict_Script_2t1/AddictScript/Data/ModderFlags.lua
@@ -0,0 +1,15 @@
+return {
+	["Invalid Info"] = player.add_modder_flag("Invalid Info"),
+	["Invalid Stats"] = player.add_modder_flag("Invalid Stats"),
+	["Godmode"] = player.add_modder_flag("Godmode"),
+	["Modded Event"] = player.add_modder_flag("Modded Event"),
+	["Invalid Script Execution"] = player.add_modder_flag("Invalid Script Execution"),
+	["Altered Migration"] = player.add_modder_flag("Altered Migration"),
+	["Modded Vehicle Mods"] = player.add_modder_flag("Modded Vehicle Mods"),
+	["Modded Explosion"] = player.add_modder_flag("Modded Explosion"),
+	["Scripted Entity Spawn"] = player.add_modder_flag("Scripted Entity Spawn"),
+	["Bad Movement"] = player.add_modder_flag("Bad Movement"),
+	["Failed Position Check"] = player.add_modder_flag("Failed Position Check"),
+	["World Entity Control"] = player.add_modder_flag("World Entity Control"),
+	["Network Sync Crash"] = player.add_modder_flag("Network Sync Crash")
+}
---
+++ Addict_Script_2t1/AddictScript/Data/NotifyColours.lua
@@ -0,0 +1,6 @@
+return {
+	["red"] = 0xff0000ff,
+	["blue"] = 0x64FA7800,
+	["green"] = 0x00ff00,
+	["orange"] = 0xff0080ff
+}
